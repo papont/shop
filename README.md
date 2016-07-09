2825e39445d31be370b6f3cd59ce851ea5f0eac2
admin <admin@admins-iMac.local>
2016-07-09 23:53:34 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Java Enterprise Online Project 
+Maven/ Spring/ Security/ JPA(Hibernate)/ REST(Jackson)/ Bootstrap(CSS)/ jQuery + plugins.
---
+++ src/main/java/ru/samara/shop/Main.java
@@ -0,0 +1,11 @@
+package ru.samara.shop;
+
+/**
+ * User: trusov
+ * Date: 09.07.2016
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello !");
+    }
+}
