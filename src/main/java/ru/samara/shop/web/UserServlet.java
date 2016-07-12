0123d118ff8921c2bc287630e6b3f9e33d5eb85f
ant76 <trusov.andrew@gmail.com>
2016-07-12 16:23:20 +0400

    Add logger

--- src/main/java/ru/samara/shop/web/UserServlet.java
+++ src/main/java/ru/samara/shop/web/UserServlet.java
@@ -1,18 +1,23 @@
 package ru.samara.shop.web;
 
+import ru.samara.shop.LoggerWrapper;
+
 import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Created by user on 12.07.2016.
- */
-//@javax.servlet.annotation.WebServlet(name = "UserServlet")
+@javax.servlet.annotation.WebServlet(name = "UserServlet", urlPatterns = "/user")
 public class UserServlet extends HttpServlet {
+    private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws javax.servlet.ServletException, IOException {
-        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+        // запрос идет прямо с сервера
+        //request.getRequestDispatcher("/userList.jsp").forward(request, response);
+        LOG.debug("Redirect to userList");
+
+        //запрос идет через браузер
+        response.sendRedirect("userList.jsp");
     }
 }
