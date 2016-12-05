6f0f5d5746d9cd469385b41f46040a9afd770f98
papont <trusoff.a@yandex.ru>
2016-12-06 02:15:33 +0400

    add Controller test

---
+++ src/test/java/ru/samara/shop/web/WebTest.java
@@ -0,0 +1,45 @@
+package ru.samara.shop.web;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import ru.samara.shop.service.UserService;
+
+import javax.annotation.PostConstruct;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-mvc.xml",
+        "classpath:spring/spring-db.xml"
+})
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+abstract public class WebTest {
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @PostConstruct
+    void postConstruct() {
+//        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Before
+    public void setUp() {
+        userService.evictCache();
+    }
+}
---
+++ src/test/java/ru/samara/shop/web/user/UserControllerTest.java
@@ -0,0 +1,37 @@
+package ru.samara.shop.web.user;
+
+import org.junit.Test;
+import org.springframework.test.context.ActiveProfiles;
+import ru.samara.shop.web.WebTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+import static org.hamcrest.Matchers.*;
+import static ru.samara.shop.Profiles.POSTGRES;
+import static ru.samara.shop.UserTestData.*;
+import static ru.samara.shop.model.BaseEntity.START_SEQ;
+import static ru.samara.shop.Profiles.DATAJPA;
+import static ru.samara.shop.Profiles.HSQLDB;
+
+@ActiveProfiles({POSTGRES, DATAJPA})
+public class UserControllerTest extends WebTest {
+
+    @Test
+    public void testUserList() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("userList"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/userList.jsp"))
+                .andExpect(model().attribute("userList", hasSize(2)))
+                .andExpect(model().attribute("mealList", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ)),
+                                hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+}
